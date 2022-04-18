4c5f5877ee8b9b1491b6476a415948416d8af9d0
Tumi Oguntola <tumi.oguntola@gmail.com>
2022-04-18 16:44:18 -0400

    Added autonomous ball orientation pickup command

---
+++ src/main/java/frc/robot/commands/autonomous/DriveToPickupAim.java
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.autonomous;
+
+import frc.robot.subsystems.Shooter;
+import frc.robot.commands.Climber.LowerArms;
+import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Index;
+import frc.robot.subsystems.Intake;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+
+/** An example command that uses an example subsystem. */
+public class DriveToPickupAim extends ParallelCommandGroup {
+    public DriveToPickupAim(Drivetrain drivetrain, Index index, Shooter shooter, Climber climber, Intake intake, double duration, double driveSpeed) {
+      super(
+        new AutoBallOrientation(drivetrain, index, duration),
+        new PickupBalls(intake, duration),
+        new LowerArms(climber)
+      );
+    }
+}
