19ad452bab50653da9bbc930808d4e6c1eebaa1a
Tumi Oguntola <tumi.oguntola@gmail.com>
2022-06-07 19:40:39 -0400

    Intake command for lowering and then running attached intake

---
+++ src/main/java/frc/robot/commands/Intake/IntakeActivation.java
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Intake;
+
+import frc.robot.subsystems.Shooter;
+import frc.robot.commands.Climber.LowerArms;
+import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Index;
+import frc.robot.subsystems.Intake;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+/** An example command that uses an example subsystem. */
+public class IntakeActivation extends SequentialCommandGroup {
+    
+  public IntakeActivation(Climber climber, Intake intake) {
+      super(
+        new InstantCommand(climber::lowerArms, climber),
+        new IntakeBalls(intake)
+      );
+  }
+
+  
+}
