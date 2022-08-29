6bb31fdffe9aaf07f2363a11d873162e22953a8d
Tumi Oguntola <tumi.oguntola@gmail.com>
2022-08-29 13:59:19 -0400

    Extensive commenting and documentation of the Index subsystem
    Signed-off-by: Tumi Oguntola <tumi.oguntola@gmail.com>

--- src/main/java/frc/robot/subsystems/Index.java
+++ src/main/java/frc/robot/subsystems/Index.java
@@ -4,101 +4,133 @@
 
 package frc.robot.subsystems;
 
-/** Add your docs here. */
-
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.CANSparkMaxLowLevel.PeriodicFrame;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import static frc.robot.GlobalVars.Index.*;
 
-public class Index extends SubsystemBase { // Made By Caputo & Oguntola
+public class Index extends SubsystemBase {  // Made By Caputo & Oguntola
   
+  // Initialize SparkMax Object
   public CANSparkMax indexMotor = new CANSparkMax(7, MotorType.kBrushless);
 
+  // Initialize boolean variables for tracking Cargo movement through index
+  public boolean CargoInTransit = false;
+  public boolean CargoExiting = false;
+  public boolean CargoPassed = false;
 
-  public boolean BallInTransit = false;
-  public boolean BallExiting = false;
-  public boolean ballPassed = false;
-
-  public double ballCounter = 0;
+  // Initialize integer variable for counting cargo in index
+  public double CargoCounter = 0;
   
+  // Initialize DigitalInput objects for reading infrared sensor values
   private static DigitalInput sensorAlpha = new DigitalInput(0);
   private static DigitalInput sensorBeta = new DigitalInput(1);
   private static DigitalInput sensorSigma = new DigitalInput(2);
 
+  // Intialize DigitalInput array for index based access of sensor values
   private static DigitalInput[] sensors = new DigitalInput[3];
 
   
   
   
-  /** Creates a new SparkTest. */
+  /** Creates a new Index subsystem. */
   public Index() {
+    // Set each array position the value of intialized sensors
     sensors[0] = sensorAlpha;
     sensors[1] = sensorBeta;
     sensors[2] = sensorSigma;
+
+    // Invert movement of index motor
     indexMotor.setInverted(true);
+
+    // Reduce CANFrame frequency of the index
     indexMotor.setPeriodicFramePeriod(PeriodicFrame.kStatus1, 30);
     indexMotor.setPeriodicFramePeriod(PeriodicFrame.kStatus2, 30);
    
   }
   
+
+  
+  /** Gets boolean of object presence at each sensor based off index in array */
   public boolean getSensor(int sensorNumber) {
     return !sensors[sensorNumber].get();
   }
 
+  /** Get value of object presence at intake sensor */
   public boolean getIntakeSensor() {
     return !sensorSigma.get();
   }
 
+  /** Get value of object presence at middle sensor */
   public boolean getMidSensor() {
     return !sensorBeta.get();
   }
 
+  /** Get value of object presence at top index sensor */
   public boolean getTopSensor() {
     return !sensorAlpha.get();
   } 
- 
+  
+
+
+  /** Set index motor speed to move cargo in index */
   public void runIndex() {
     indexMotor.set(0.65);
   }
 
-  public void removeBalls() {
+  /** Set index motor speed to eject cargo from index */
+  public void removeCargo() {
     indexMotor.set(-0.6);
   }
 
-  public void feedBalls() {
+  /** Set index motor speed to intake cargo into index */
+  public void feedCargo() {
     indexMotor.set(0.4);
   }
 
+  /** Stop index motor */
   public void stopIndex() {
     indexMotor.stopMotor();
   }
 
+  /** Check if the index moor is moving at greater than 10% power */
   public boolean isRunning() {
     return (Math.abs(indexMotor.get()) > 0.1);
   }
 
-  public void startWithTwoBalls() {
-    ballCounter = 2;
+
+
+
+  /** Set cargo counter to 2 (Meant for running autonomous commands at the beginning of a match) */
+  public void startWithTwoCargo() {
+    CargoCounter = 2;
   }
 
-  public void startWithOneBall() {
-    ballCounter = 1;
+  /** Set cargo counter to 1 (Meant for running autonomous commands at the beginning of a match) */
+  public void startWithOneCargo() {
+    CargoCounter = 1;
   }
 
-  public void startWithNoBalls() {
-    ballCounter = 0;
+  /** Set cargo counter to 0 (Meant for running autonomous commands at the beginning of a match) */
+  public void startWithNoCargo() {
+    CargoCounter = 0;
   }
 
 
+
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    BallCounter_Entry.setDouble(ballCounter);
 
+    // Set NetworkTableEntry to value of cargo counted
+    CargoCounter_Entry.setDouble(CargoCounter);
+
+    // Set IndexRunning boolean to true when the index is running, and false if otherwise
     if (Math.abs(indexMotor.get()) > 0.1) {
       IndexRunning = true;
     }
